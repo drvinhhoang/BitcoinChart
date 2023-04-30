4d87c582695520f339e2e5c9b7ec3cc8fbfbe346
vinhhoang <dr.vinhhoang@gmail.com>
2023-04-30 14:15:42 +0700

    Initial Commit

---
+++ BitcoinChart/BitcoinChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitcoinChartApp.swift
+//  BitcoinChart
+//
+//  Created by VinhHoang on 30/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct BitcoinChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
